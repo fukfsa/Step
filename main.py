5a48b363490b93292be6425afb12861401f4f416
fukfsa <zxcvbnm20130314@aliyun.com>
2022-04-25 16:53:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ def main(user, passwd, step):
         return
 
     if step == '':
-        print("已设置为随机步数（20000-29999）")
+        print("已设置为随机步数（10000-19999）")
         step = str(random.randint(10000, 19999))
     login_token = 0
     login_token, userid = login(user, password)
